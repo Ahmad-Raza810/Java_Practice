5ff6643084188617402a0043e3e695525ff9539f
Ahmad raza <mustafaraza03898@gmail.com>
2025-01-01 11:51:53 +0530

    giving explanation of TestString class

--- JavaPractice/TestString.java
+++ JavaPractice/TestString.java
@@ -3,6 +3,13 @@ package JavaPractice;
 public class TestString {
     public static void main(String[] args) {
 
+         // string class is final and immutable class
+        //all object by default storing in heap area
+        //in heap area there is one moreplace
+        // {heap area(string constant pool)}
+        //string litral--->in scp
+        //string constructor ---> one in heap and one in scp
+
 
         String str1 = new String("This is Bond");
         String str3 = new String("This is Bond");
