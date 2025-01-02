6cadde7271e2408ed5b758f7f8d5ac2d1f4a6af5
Ahmad raza <mustafaraza03898@gmail.com>
2025-01-02 10:32:28 +0530

    adding string methods part 3

--- JavaPractice/StringMethods.java
+++ JavaPractice/StringMethods.java
@@ -12,6 +12,7 @@ public class StringMethods {
 
         System.out.println("comparison with Cheking case:"+userName.equals(newUser));
         System.out.println("comparison without Cheking case:"+userName.equalsIgnoreCase(newUser));
+        
         System.out.println("\n___________________________________________________________________________________\n");
 
         String s1="java";
@@ -50,5 +51,31 @@ public class StringMethods {
         //below method remove spaces from begining and ending of a string.........
         // name.trim()
 
+        //Replaces all occurrences of a specified character with another character.
+        //It works on all occurrences of the character or substring in the string.
+        //if speciefied substring is not present in current string then it return current string(no error or exception)
+        String newName=name.replace("Ahmad","Mohammad");
+        System.out.println("my new name is: "+newName);
+
+        System.out.println(name.contains("Raza"));
+
+        System.out.println(name.startsWith("Ahm"));
+
+        System.out.println(name.endsWith("Am"));
+
+        System.out.println("\n_____________________________________________________________________________________________\n");
+
+        String check="   ";
+        System.out.println(check.isEmpty());
+        System.out.println(check.isBlank());
+
+        System.out.println(name.indexOf("mad"));
+        System.out.println(name.lastIndexOf("Raz"));
+
+        double d=807;
+         String d2=String.valueOf(d);
+
+        System.out.println(d2 instanceof String);
+        //System.out.println(d instanceof  double);
     }
 }
