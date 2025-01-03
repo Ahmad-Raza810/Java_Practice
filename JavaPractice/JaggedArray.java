c9ea91c46169ff2c769d294f675458901514af01
Ahmad raza <mustafaraza03898@gmail.com>
2025-01-03 22:50:47 +0530

    anonyms array

--- JavaPractice/JaggedArray.java
+++ JavaPractice/JaggedArray.java
@@ -5,26 +5,41 @@ public class JaggedArray {
         //an array where each element is an array, and the inner arrays can have different lengths.
         //all rows have not similar number of coulmn.
 
-        String [][]s=new String[3][];
+        String[][] s = new String[3][];
 
         //System.out.println(s[0][0]);
 
-        s[0]=new String[]{"Python","Java"};
-        s[1]=new String[]{"Js","Typescript","Php"};
-        s[2]=new String[]{"GoLang","Ruby","HTML","Matlab"};
+        s[0] = new String[]{"Python", "Java"};
+        s[1] = new String[]{"Js", "Typescript", "Php"};
+        s[2] = new String[]{"GoLang", "Ruby", "HTML", "Matlab"};
 
-        String max="";
-        for (String []Languagues:s)
-        {
-           for (String language:Languagues)
-           {
-             if (language.length()>max.length())
-             {
-                 max=language;
-             }
-           }
+        String max = "";
+        for (String[] Languagues : s) {
+            for (String language : Languagues) {
+                if (language.length() > max.length()) {
+                    max = language;
+                }
+            }
             System.out.println();
         }
-        System.out.println("the longest element (length wise) : "+max);
+        System.out.println("the longest element (length wise) : " + max);
+
+
+        //Anonyms Array
+        //No variable reference: It does not have a name.
+        //Immediate use: It is often passed directly to a method or used in a context where its lifespan is short.
+        // Cannot be reused: Once the anonymous array is used, it cannot be accessed again.
+        printArray(new int[]{10,20,30,40});
+
+
     }
+
+    static void printArray(int[] a) {
+        if (a != null) {
+            for (int i : a) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
 }
